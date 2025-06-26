78b90ae8eb2da6253a6d159e2cdbb4c1b378fa74
KaylanDev <k1ndealblack@gmail.com>
2025-06-25 23:59:23 -0300

    teste: criado testes  e teste de user arrumado

---
+++ TesteXunit/RepositoryTestexUnit/UserxUnit.cs
@@ -0,0 +1,43 @@
+﻿using Blog.Infrastruture.Context;
+using Blog.Infrastruture.Repository;
+using Blog_Domain.Repository;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteXunit.RepositoryTestexUnit
+{
+    public class UserxUnit : IDisposable
+    {
+        public readonly IUserRepository _repository;
+        public static DbContextOptions<AppDbContext> contextoptions;
+        private AppDbContext Context; // Propriedade estática para acessar o contexto do banco de dados
+
+
+
+        private const string conectionString = "Data Source=SHEISLINDA;Initial Catalog=BlogDB;Integrated Security=True;Trust Server Certificate=True";
+
+        static UserxUnit()
+        {
+            contextoptions = new DbContextOptionsBuilder<AppDbContext>().UseSqlServer(conectionString).Options;
+        }
+
+        public UserxUnit()
+        {
+            Context = new AppDbContext(contextoptions);
+            Context.Database.EnsureDeleted(); // Limpa o banco de dados antes de cada teste
+            Context.Database.EnsureCreated(); // Cria o banco de dados antes de cada teste
+            _repository = new UserRepository(Context);
+        }
+
+
+        public void Dispose()
+        {
+            Context.Dispose(); // Libera recursos no final da execução de todos os testes da classe
+        }
+    }
+}
---
+++ TesteXunit/UserTeste/PostUserTeste.cs
@@ -0,0 +1,48 @@
+﻿using Blog.API.Controllers;
+using Blog.Application.Interfaces;
+using Blog.Application.Services;
+using Blog_Domain.Models;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TesteXunit.RepositoryTestexUnit;
+
+namespace TesteXunit.UserTeste
+{
+    public class PostUserTeste : IClassFixture<UserxUnit>
+    {
+        private readonly UserController _controller;
+
+        public PostUserTeste(UserxUnit userxUnit)
+        {
+            _controller = new UserController(new UserService(userxUnit._repository));
+        }
+
+        [Fact]
+     public async Task Get_Ok_Result()
+        {
+            //act
+            var result = await _controller.Get();
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<OkObjectResult>();
+            
+        }
+
+        [Fact]
+        public async Task GetById_Ok_Result()
+        {
+            // Arrange
+            int id = 1; // Certifique-se de que este ID existe no banco de dados de teste
+            // Act
+            var result = await _controller.GetById(id);
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<OkObjectResult>();
+        }
+    }
+}
