df79652478e5829647fe86a0dafbc001b2a6153b
KaylanDev <k1ndealblack@gmail.com>
2025-07-03 22:38:04 -0300

    feat: criado padrao Result

---
+++ BlogAPI/ExceptionHandler/Result.cs
@@ -0,0 +1,18 @@
+﻿namespace Blog.API.ExceptionHandler;
+
+public class Result
+{
+    public bool IsSuccess { get; }
+    public List<string>? ErrorMessage { get; } = new List<string>();
+    public object? Value { get; }
+
+    protected Result(bool isSuccess, string? errorMessage, object? value)
+    {
+        IsSuccess = isSuccess;
+        if (!string.IsNullOrEmpty(errorMessage)) ErrorMessage.Add(errorMessage);
+        Value = value;
+    }
+
+    public static Result Success(object? value = null) => new Result(true, null, value);
+    public static Result Failure(string errorMessage)=> new Result(false, errorMessage, null);
+}
